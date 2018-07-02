ae10825b4751564b32cec50d8a40f63e3ea1dc3e
Sonabayim <huseynzade.sonabeyim@gmail.com>
2018-07-02 18:40:26 +0400

    changes

--- testing-countme/example-python.py
+++ testing-countme/example-python.py
@@ -14,99 +14,86 @@ from array import array
 active = True
 
 while(1):
-    if active == True:
-       
-        file = urllib.request.urlopen('http://192.168.5.147/xml')
-        data = file.read()
-        file.close()
+	if active == True:
+	   
+		file = urllib.request.urlopen('http://192.168.5.149/xml')
+		data = file.read()
+		file.close()
 
-        data = xmltodict.parse(data)
-        
-        length = len(data['response'])
-        # print(data['response'])
-        sensor = data['response']
-        print(type(sensor))
-        sensor1 = map(str.strip, sensor.split(','))
-        #text = 
-        # sensor = sensor.split(',')
-        # array = array('t' ,sensor)
-        print(map(str.strip, sensor.split(',')))
-        # print(sensor[2])
-        Z = [0] * 64
-        for x in range(0,64):
-            t = list(sensor[6*x] + sensor[6*x+1] + sensor[6*x+2] + sensor[6*x+3] + sensor[6*x+4])
-            t = float(''.join(t))
-            Z[x] = t
-        print(len(Z))
-        # print(t)
-        # print(t + 5)
-        # print(type(sensor1))
+		data = xmltodict.parse(data)
+		
+		length = len(data['response'])
+		# print(data['response'])
+		sensor = data['response']
+		# print(type(sensor))
+		sensor1 = map(str.strip, sensor.split(','))
+	
 
-        # Read pixels, convert them to values between 0 and 1, map them to an 8x8 grid
-        pixels = Z
-        pixmax = max(pixels)
-        pixels = [x / pixmax for x in pixels]
-        points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-        grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+		Z = [0] * 64
+		for x in range(0,64):
+			t = list(sensor[6*x] + sensor[6*x+1] + sensor[6*x+2] + sensor[6*x+3] + sensor[6*x+4])
+			t = float(''.join(t))
+			Z[x] = t
+		# print(len(Z))
+		# print(t)
+		# print(t + 5)
+		# print(type(sensor1))
 
-        # bicubic interpolation of 8x8 grid to make a 32x32 grid
-        bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
-        image = np.array(bicubic)
-        image = np.reshape(image, (32, 32))
-        print(image)
-        plt.imsave('color_img.png', image)
+		# Read pixels, convert them to values between 0 and 1, map them to an 8x8 grid
+		pixels = Z
+		pixmax = max(pixels)
+		pixels = [x / pixmax for x in pixels]
+		points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+		grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
 
-        # Read image
-        img = cv2.imread("color_img.png", cv2.IMREAD_GRAYSCALE)
-        img = cv2.bitwise_not(img)
+		# bicubic interpolation of 8x8 grid to make a 32x32 grid
+		bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
+		image = np.array(bicubic)
+		image = np.reshape(image, (32, 32))
+		print(image)
+		plt.imsave('color_img.png', image)
 
-        # Setup SimpleBlobDetector parameters.
-        params = cv2.SimpleBlobDetector_Params()
+		# Read image
+		img = cv2.imread("color_img.png", cv2.IMREAD_GRAYSCALE)
+		img = cv2.bitwise_not(img)
 
-        # Change thresholds
-        params.minThreshold = 10
-        params.maxThreshold = 255
+		# Setup SimpleBlobDetector parameters.
+		params = cv2.SimpleBlobDetector_Params()
 
-        # Filter by Area.
-        params.filterByArea = True
-        params.minArea = 5
+		# Change thresholds
+		params.minThreshold = 10
+		params.maxThreshold = 255
 
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = 0.1
+		# Filter by Area.
+		params.filterByArea = True
+		params.minArea = 5
 
-        # Filter by Convexity
-        params.filterByConvexity = False
-        params.minConvexity = 0.87
+		# Filter by Circularity
+		params.filterByCircularity = True
+		params.minCircularity = 0.1
 
-        # Filter by Inertia
-        params.filterByInertia = False
-        params.minInertiaRatio = 0.01
+		# Filter by Convexity
+		params.filterByConvexity = False
+		params.minConvexity = 0.87
 
-        # Set up the detector with default parameters.
-        detector = cv2.SimpleBlobDetector_create(params)
+		# Filter by Inertia
+		params.filterByInertia = False
+		params.minInertiaRatio = 0.01
 
-        # Detect blobs.
-        keypoints = detector.detect(img)
+		# Set up the detector with default parameters.
+		detector = cv2.SimpleBlobDetector_create(params)
 
-        for i in range (0, len(keypoints)):
-            x = keypoints[i].pt[0]
-            y = keypoints[i].pt[1]
-            print(x, y)
-
-        # Get the channel setting for interval_ms
-        # interval = config.get("channel.interval_ms")
-        # interval = config.get("channel.active")
-        # Report the device having set interval_ms
-        # config.set("channel.interval_ms", interval)
-        # config.set("channel.active", active)
-        # channel.send(str(len(keypoints)))
-        # time.sleep(interval)
-        # time.sleep(5)
-
-    else:
-        print("idle")
-        # channel.send("Idle")
-        # time.sleep(5)
-        # interval = config.get("channel.active")
-        # config.set("channel.active", active)
+		# Detect blobs.
+		keypoints = detector.detect(img)
+		for i in range (0, len(keypoints)):
+			x = keypoints[i].pt[0]
+			y = keypoints[i].pt[1]
+			print("People :" + str(len(keypoints)))
+	
+		
+	else:
+		print("idle")
+		# channel.send("Idle")
+		# time.sleep(5)
+		# interval = config.get("channel.active")
+		# config.set("channel.active", active)
